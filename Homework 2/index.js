156d9ecb2f5546dca61c7e5d4887bb53a8a37376
Hsing-Tzu <daisyko1234@gmail.com>
2022-12-10 16:45:45 +0800

    Update index.js

--- Homework 2/index.js
+++ Homework 2/index.js
@@ -9,12 +9,43 @@ function clearScreen() {
     }
     
     // This function evaluates the expression and returns result
-    function calculate() {
+    /* function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
-    }
+    }*/
     function backspace() {
         var value = document.getElementById("result").value;
         document.getElementById("result").value = value.substr(0, value.length - 1);
+    }
+
+
+    function operate(){
+		var res = [];
+		var answer;
+		
+		if(document.getElementById("result").value.includes("+")){
+		res = document.getElementById("result").value.split('+');
+		return parseInt(res[0]) + parseInt(res[1]);
+		}
+		
+		else if(document.getElementById("result").value.includes("-")){
+		res = document.getElementById("result").value.split('-');
+		return parseInt(res[0]) - parseInt(res[1]);
+		}
+		
+		else if(document.getElementById("result").value.includes("/")){
+		res = document.getElementById("result").value.split('/');
+		return parseInt(res[0]) / parseInt(res[1]);
+		}
+		
+		else{
+		res = document.getElementById("result").value.split('*');
+		return answer= parseInt(res[0]) * parseInt(res[1]);
+		}
+    }   
+		
+    function calculate(){	
+        var a = operate()
+        document.getElementById('result').value = a; 
     }

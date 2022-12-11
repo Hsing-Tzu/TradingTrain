ea0a963f8d59f2e4572c3af1d5aea4d76350503c
Hsing-Tzu <daisyko1234@gmail.com>
2022-12-11 10:45:19 +0800

    update

--- Homework 2/index.js
+++ Homework 2/index.js
@@ -49,3 +49,41 @@ function clearScreen() {
         var a = operate()
         document.getElementById('result').value = a; 
     }
+
+        // Keyboard input
+        document.addEventListener('keydown', (e) => {
+            if (e.code == "Enter") { calculate() } 
+            else if (e.code == "Backspace") { backspace() }
+            else if (e.code == "Delete") { clearScreen() }
+            else { input(e) } 
+       })
+
+       function input(e) {
+        // initialize inputValue
+        let inputValue = "";
+
+        // If click, set inputValue as previously
+        if (e.type == "click") {
+            inputValue = e.target.innerText;
+        }
+
+        // Otherwise set input by key pressed 
+        else{
+        if (e.key == "0") {inputValue = "0"}
+        else if (e.key == "1") {inputValue = "1"}
+        else if (e.key == "2") {inputValue = "2"}
+        else if (e.key == "3") {inputValue = "3"}
+        else if (e.key == "4") {inputValue = "4"}
+        else if (e.key == "5") {inputValue = "5"}
+        else if (e.key == "6") {inputValue = "6"}
+        else if(e.key == "7") {inputValue = "7"}
+        else if(e.key == "8") {inputValue = "8"}
+        else if(e.key == "9") {inputValue = "9"}
+        else if(e.key == "+") {inputValue = "+"}
+        else if(e.key == "-") {inputValue = "-"}
+        else if(e.key == "*") {inputValue = "*"}
+        else if(e.key == "/") {inputValue = "/"}
+        }
+
+        document.getElementById('result').value += inputValue;
+    }

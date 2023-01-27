900f1eaccbf44a4e6f9d4caf2b5744ce4accf7b9
Hsing-Tzu <daisyko1234@gmail.com>
2023-01-27 21:01:30 +0800

    update

---
+++ Homework 5/data/lastnews.js
@@ -0,0 +1,5 @@
+const lastnews = {
+    src :"https://images.freeimages.com/images/large-previews/4b2/walking-on-the-mountain-1-1566594.jpg",
+    href :"",
+    text :"新聞標題新聞標題新聞標題新聞標題新聞標題"
+};
---
+++ Homework 5/script/renderlastnews.js
@@ -0,0 +1,6 @@
+const aElement = document.getElementById("list-news__last");
+const imgElement = document.getElementById("list-news__last__image");
+const textElement = document.getElementById("list-news__last__text");
+aElement.href = lastnews.href;
+imgElement.src = lastnews.src;
+textElement.text = lastnews.text;

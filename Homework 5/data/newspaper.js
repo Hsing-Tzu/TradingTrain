8a52270f0f6ac12f9df1ea2c0aec5bb04d9daf06
Hsing-Tzu <daisyko1234@gmail.com>
2023-02-05 02:23:50 +0800

    update

---
+++ Homework 5/data/newspaper.js
@@ -0,0 +1,4 @@
+const newspaper = {
+    src:"https://images.freeimages.com/images/large-previews/f3b/mixer-1641816.jpg",
+    href :"",
+}
---
+++ Homework 5/script/rendernewspaper.js
@@ -0,0 +1,4 @@
+const npElement = document.getElementById("newspaper-news__image");
+const imgnpElement = document.getElementById("newspaper");
+npElement.href = newspaper.href;
+imgnpElement.src = newspaper.src;

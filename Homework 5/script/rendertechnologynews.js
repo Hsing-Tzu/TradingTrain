a4ec347edec963422a7561975c9aaeaf627ec01f
Hsing-Tzu <daisyko1234@gmail.com>
2023-02-12 19:44:52 +0800

    update

---
+++ Homework 5/script/rendertechnologynews.js
@@ -0,0 +1,15 @@
+const tc1Element = document.getElementById("technology1");
+const imgtc1Element = document.getElementById("technology__image1");
+const titletc1Element = document.getElementById("tctitle1");
+const texttc1Element = document.getElementById("tctext1");
+const tc2Element = document.getElementById("technology2");
+const imgtc2Element = document.getElementById("technology__image2");
+const titletc2Element = document.getElementById("tctitle2");
+const texttc2Element = document.getElementById("tctext2");
+
+imgtc1Element.src = technologynews.src1;
+titletc1Element.innerText = technologynews.title1;
+texttc1Element.innerText = technologynews.text1;
+imgtc2Element.src = technologynews.src2;
+titletc2Element.innerText = technologynews.title2;
+texttc2Element.innerText = technologynews.text2;

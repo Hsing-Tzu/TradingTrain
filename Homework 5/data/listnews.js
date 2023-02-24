0447482abe566edf5194214e42723a649354824d
Hsing-Tzu <daisyko1234@gmail.com>
2023-02-24 15:56:04 +0800

    update

---
+++ Homework 5/data/listnews.js
@@ -0,0 +1,33 @@
+const leftnews = [
+    {
+        src:"",
+        href :"1 ",
+        text :"新聞標題1 新聞標題新聞標題新聞標題新聞標題"
+    },
+    {
+        href :"1 ",
+        text :"新聞標題2 新聞標題新聞標題新聞標題新聞標題"
+    },
+    {
+        href :"1 ",
+        text :"新聞標題3 新聞標題新聞標題新聞標題新聞標題"
+    },
+    {
+        href :"1 ",
+        text :"新聞標題4 新聞標題新聞標題新聞標題新聞標題"
+    },
+];
+
+const mainnews = {
+    src :"https://images.freeimages.com/images/large-previews/4b2/walking-on-the-mountain-1-1566594.jpg",
+    href :"",
+    text :"新聞標題新聞標題新聞標題新聞標題新聞標題"
+};
+
+const lastnews = {
+    src :"https://images.freeimages.com/images/large-previews/e5c/mill-house-1637156.jpg",
+    href :"",
+    title :"新聞標題新聞標題新聞標題新聞標題新聞標題",
+    text:"新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容"
+};
+
---
+++ Homework 5/script/renderlistnews.js
@@ -0,0 +1,26 @@
+const leftElements = [...document.getElementsByClassName('story-list__text') ];
+leftElements.forEach((leftElement, idx) => {
+  const aElement = document.createElement('a');
+  const imgElement = document.createElement('img');
+  aElement.href = leftnews[idx].href;
+  aElement.innerText = leftnews[idx].text;
+  imgElement.src = leftnews[idx].src;
+  leftElement.appendChild(imgElement);
+  leftElement.appendChild(aElement);
+});
+
+const aElement = document.getElementById("list-news__center");
+const imgElement = document.getElementById("list-news__center__image");
+const textElement = document.getElementById("list-news__center__text");
+aElement.href = mainnews.href;
+imgElement.src = mainnews.src;
+textElement.innerText = mainnews.text;
+
+const bElement = document.getElementById("list-news__last");
+const imgbElement = document.getElementById("list-news__last__image");
+const titlebElement = document.getElementById("list-news__last__title");
+const textbElement = document.getElementById("list-news__last__text");
+bElement.href = lastnews.href;
+imgbElement.src = lastnews.src;
+titlebElement.innerText = lastnews.title;
+textbElement.innerText = lastnews.text;

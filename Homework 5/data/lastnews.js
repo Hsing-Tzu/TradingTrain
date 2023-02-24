d4bd07e154832670e58e2a93527b740dd02dcf14
Hsing-Tzu <daisyko1234@gmail.com>
2023-02-24 18:16:56 +0800

    update

---
+++ Homework 5/data/lastnews.js
@@ -0,0 +1,7 @@
+const lastnews = {
+    src :"https://images.freeimages.com/images/large-previews/e5c/mill-house-1637156.jpg",
+    href :"",
+    title :"新聞標題新聞標題新聞標題新聞標題新聞標題",
+    text:"新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容新聞內容"
+};
+

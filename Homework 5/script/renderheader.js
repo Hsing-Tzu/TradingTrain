d5c3f44f2d917f795675e0ab2bc8a139f0f94ac0
Hsing-Tzu <daisyko1234@gmail.com>
2023-03-12 15:41:43 +0800

    update

--- Homework 5/script/renderheader.js
+++ Homework 5/script/renderheader.js
@@ -5,3 +5,10 @@ for (const item of headerlist){
     aElement.href = item.href;
     header.appendChild(aElement);
 };
+const header1 = document.getElementById("menu1");
+for (const item of headerlist){
+    const aElement = document.createElement("a");
+    aElement.innerText = item.name;
+    aElement.href = item.href;
+    header1.appendChild(aElement);
+};

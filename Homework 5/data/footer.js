5598381e5dc3a28e5e861e60f8e3ff3c87f18c04
Hsing-Tzu <daisyko1234@gmail.com>
2023-03-12 15:24:20 +0800

    update

--- Homework 5/data/footer.js
+++ Homework 5/data/footer.js
@@ -15,4 +15,30 @@ const footerlist = [
     src: './image/YT.png',
     href: "",
     },
+];
+const note = [
+    {
+        name: "產品介紹",
+        href: ""
+    },
+    {
+        name: "  常見問題",
+        href: ""
+    },
+    {
+        name: "  我要發問",
+        href: ""
+    },
+    {
+        name: "  企業團購",
+        href: ""
+    },
+    {
+        name: "  隱私政策 & 服務條款",
+        href: ""
+    },
+    {
+        name: "  聯合新聞網",
+        href: ""
+    },
 ]
--- Homework 5/script/renderfooter.js
+++ Homework 5/script/renderfooter.js
@@ -1,9 +1,17 @@
-const footer = document.getElementById("footer");
+const sociallist = document.getElementById("social-list");
 for (const item of footerlist){
     const aElement = document.createElement("a");
     const imgElement = document.createElement("img");
     imgElement.src = item.src;
     aElement.href = item.href;
     aElement.appendChild(imgElement);
-    footer.appendChild(aElement);
+    sociallist.appendChild(aElement);
+};
+
+const footernote = document.getElementById("footer-note");
+for (const item of note){
+    const aElement = document.createElement("a");
+    aElement.innerText = item.name;
+    aElement.href = item.href;
+    footernote.appendChild(aElement);
 };
